cf61e80bc77cced2261e784afe30368408fe9274
npocmop <npocmop@c44a8926-5273-11de-936b-e9512c4d2277>
2009-06-07 15:30:27 +0000

    wp-themes

--- wp-plugins/drain-hole/view/drain-hole/top_downloads.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><ul>
-<?php foreach ($files AS $file) : $hole = DH_Hole::get ($file->hole_id); ?>
-	<li><?php echo $file->url ($hole); ?> (<?php echo $file->hits ?>)</li>
-<?php endforeach; ?>
-</ul>
