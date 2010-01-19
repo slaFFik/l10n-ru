cdfdeb78ed318e4e4b8df23e3e8bb54a8f9b4a5f
npocmop <npocmop@c44a8926-5273-11de-936b-e9512c4d2277>
2010-01-19 18:46:42 +0000

    MU 2.9.1.1

---
+++ tags/wordpress-mu/2.9.1.1/ru_RU.php
@@ -0,0 +1,22 @@
+<?php
+// Make the menu wider and correct some overlapping issues
+function ru_accomodate_markup() {
+	global $locale, $wp_styles;
+
+	wp_enqueue_style($locale, WP_CONTENT_URL . "/languages/$locale.css", array(), '20090128', 'all');
+	wp_enqueue_style("$locale-ie", WP_CONTENT_URL . "/languages/$locale-ie.css", array(), '20090128', 'all');
+	$wp_styles->add_data("$locale-ie", 'conditional', 'IE');
+
+	wp_print_styles();
+}
+add_action('admin_head', 'ru_accomodate_markup');
+
+function ru_restore_scripts_l10n() {
+	global $wp_scripts;
+
+	if ( is_a($wp_scripts, 'WP_Scripts') ) {
+		do_action_ref_array('wp_default_scripts', array(&$wp_scripts));
+	}
+}
+add_action('init', 'ru_restore_scripts_l10n');
+?>
