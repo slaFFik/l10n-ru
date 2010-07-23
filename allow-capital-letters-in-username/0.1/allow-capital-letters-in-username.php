38cc72ed17411a417d2a02d20e3cf320b053deab
sergeybiryukov.ru <sergeybiryukov.ru@c44a8926-5273-11de-936b-e9512c4d2277>
2010-07-23 11:09:51 +0000

    updated description

--- allow-capital-letters-in-username/0.1/allow-capital-letters-in-username.php
+++ allow-capital-letters-in-username/0.1/allow-capital-letters-in-username.php
@@ -3,7 +3,7 @@
 Plugin Name: Allow Capital Letters In Username (BuddyPress)
 Version: 0.1
 Plugin URI: http://ru.forums.wordpress.org/topic/3738
-Description: Allows to use uppercase latin letters when registering a new user.
+Description: Allows to use uppercase Latin letters when registering a new user.
 Author: Sergey Biryukov
 Author URI: http://sergeybiryukov.ru/
 */
